21fcb12986e8c185586b0b300b381d1f6d1d3b2f
You Song Hou <ysongweb3@gmail.com>
2024-10-25 22:28:22 -0400

    refactor: update Wordle Nada program to return an array

--- nada/src/wordle.py
+++ nada/src/wordle.py
@@ -4,6 +4,7 @@ def check_match(value_1: SecretInteger, value_2: Integer) -> Boolean:
     return value_1 == value_2
 
 def nada_main():
+    size = 3
     party1 = Party(name="Party1")
 
     letter_1 = SecretInteger(Input(name="letter_1", party=party1))
@@ -13,9 +14,12 @@ def nada_main():
     guess_letter_2 = SecretInteger(Input(name="guess_letter_2", party=party1))
     guess_letter_3 = SecretInteger(Input(name="guess_letter_3", party=party1))
 
-    number_of_correct = Integer(0)
-    number_of_correct += check_match(letter_1, guess_letter_1).if_else(Integer(1), Integer(0))
-    number_of_correct += check_match(letter_2, guess_letter_2).if_else(Integer(1), Integer(0))
-    number_of_correct += check_match(letter_3, guess_letter_3).if_else(Integer(1), Integer(0))
+    result = []
+    result.append(check_match(letter_1, guess_letter_1).if_else(Integer(1), Integer(0)))
+    result.append(check_match(letter_2, guess_letter_2).if_else(Integer(1), Integer(0)))
+    result.append(check_match(letter_3, guess_letter_3).if_else(Integer(1), Integer(0)))
 
-    return [Output(number_of_correct, "my_output", party1)]
+    outputs = [Output(result[i], "my_output_" + str(i), party1) for i in range(size)]
+
+    return outputs
+    # return [Output(number_of_correct, "my_output", party1)]
