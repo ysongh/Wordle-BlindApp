957b151659d607b1c9206f5ac42ea88ddb27e828
You Song Hou <ysongweb3@gmail.com>
2024-10-29 21:44:28 -0400

    feat: add additional inputs for Wordle Nada program

--- nada/src/wordle.py
+++ nada/src/wordle.py
@@ -1,6 +1,6 @@
 from nada_dsl import *
 
-SIZE = 3
+SIZE = 5
 
 def check_match(value_1: SecretInteger, value_2: Integer) -> Boolean:
     return value_1 == value_2
@@ -18,20 +18,28 @@ def nada_main():
     letter_1 = SecretInteger(Input(name="letter_1", party=party1))
     letter_2 = SecretInteger(Input(name="letter_2", party=party1))
     letter_3 = SecretInteger(Input(name="letter_3", party=party1))
+    letter_4 = SecretInteger(Input(name="letter_4", party=party1))
+    letter_5 = SecretInteger(Input(name="letter_5", party=party1))
     guess_letter_1 = SecretInteger(Input(name="guess_letter_1", party=party1))
     guess_letter_2 = SecretInteger(Input(name="guess_letter_2", party=party1))
     guess_letter_3 = SecretInteger(Input(name="guess_letter_3", party=party1))
+    guess_letter_4 = SecretInteger(Input(name="guess_letter_4", party=party1))
+    guess_letter_5 = SecretInteger(Input(name="guess_letter_5", party=party1))
 
     word: list[SecretInteger] = [
         letter_1,
         letter_2,
-        letter_3
+        letter_3,
+        letter_4,
+        letter_5
     ]
 
     result = []
     result.append((check_match(letter_1, guess_letter_1)).if_else(Integer(9), is_number_present_in_list(word, guess_letter_1)))
     result.append((check_match(letter_2, guess_letter_2)).if_else(Integer(9), is_number_present_in_list(word, guess_letter_2)))
     result.append((check_match(letter_3, guess_letter_3)).if_else(Integer(9), is_number_present_in_list(word, guess_letter_3)))
+    result.append((check_match(letter_4, guess_letter_4)).if_else(Integer(9), is_number_present_in_list(word, guess_letter_4)))
+    result.append((check_match(letter_5, guess_letter_5)).if_else(Integer(9), is_number_present_in_list(word, guess_letter_5)))
 
     outputs = [Output(result[i], str(i + 1), party1) for i in range(SIZE)]
 
